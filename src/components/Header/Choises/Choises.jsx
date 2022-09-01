8857c5aff4671fdff7078533d339c1a4d6bacd75
Bisquitman <lynx8309@gmail.com>
2022-09-01 13:48:08 +0300

    Day 3

--- src/components/Header/Choises/Choises.jsx
+++ src/components/Header/Choises/Choises.jsx
@@ -1,40 +1,30 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { holidaysContext } from '../../../context/holidaysContext';
 import style from './Choises.module.css';
-
-const holidays = {
-  newyear: 'Новый год',
-  birthdayWomen: 'День рождения  Ж',
-  birthdayMen: 'День рождения  М',
-  womenday: '8 марта',
-  knowledgeday: 'День знаний',
-};
 
 const Choises = () => {
   const [isOpenChoises, setIsOpenChoises] = useState(false);
-  const [holiday, setHoliday] = useState('Выбрать праздник');
+  const { holidays, holiday, changeHoliday } = useContext(holidaysContext);
 
   const toggleChoises = () => {
     setIsOpenChoises(!isOpenChoises);
   };
 
-  const changeHoliday = (title) => {
-    setHoliday(title);
-  };
-
   return (
     <div className={style.wrapper}>
-      <button className={style.button} onClick={toggleChoises}>{holiday}</button>
+      <button className={style.button} onClick={toggleChoises}>
+        {holidays[holiday] || 'Выбрать праздник'}
+      </button>
       {isOpenChoises && (
         <ul className={style.list}>
           {Object.entries(holidays).map((entry) => (
-            <li 
-              className={style.item} 
+            <li
+              className={style.item}
               key={entry[0]}
               onClick={() => {
-                changeHoliday(entry[1]);
+                changeHoliday(entry[0]);
                 toggleChoises();
-              }}
-            >
+              }}>
               {entry[1]}
             </li>
           ))}
