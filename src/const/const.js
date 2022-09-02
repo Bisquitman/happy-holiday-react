50d648eb9be64e19b1057a736c76cf6f08154a62
Bisquitman <lynx8309@gmail.com>
2022-09-02 22:26:19 +0300

    add: new API server

--- src/const/const.js
+++ src/const/const.js
@@ -1,2 +1,3 @@
-export const URI_API = 'https://vanilla-legend-factory.glitch.me/api';
 // export const URI_API = 'http://localhost:3300/api';
+// export const URI_API = 'https://vanilla-legend-factory.glitch.me/api';
+export const URI_API = 'https://boiling-bloom-blackbird.glitch.me/api';
